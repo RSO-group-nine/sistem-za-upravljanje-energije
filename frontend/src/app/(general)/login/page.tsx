1d8ac0cecab3c9210a427b9a64527197c7687535
Žiga Črv <62549216+zigac9@users.noreply.github.com>
2024-12-03 20:59:29 +0100

    Add bg to login and register

--- frontend/src/app/(general)/login/page.tsx
+++ frontend/src/app/(general)/login/page.tsx
@@ -1,20 +1,27 @@
-import LoginForm from '../components/loginForm';
-import Link from 'next/link';
+import LoginForm from "../components/loginForm";
+import Link from "next/link";
 
 export default function Login() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <LoginForm />
-        <div className="mt-4 text-center">
-            <Link
-                href='/register'
-                className="text-blue-600 hover:underline"
-            >
-                Register
+    <>
+      <div
+        className="min-h-screen flex items-center justify-center bg-cover bg-center"
+        style={{
+          backgroundImage: `url('../../../../img/background.jpg')`,
+        }}
+      >
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
+          <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
+            Energy Management System
+          </h1>
+          <LoginForm />
+          <div className="mt-4 text-center">
+            <Link href="/register" className="text-blue-600 hover:underline">
+              Register
             </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- frontend/src/app/(general)/register/page.tsx
+++ frontend/src/app/(general)/register/page.tsx
@@ -1,20 +1,27 @@
-import RegistrationForm from '../components/registrationForm';
-import Link from 'next/link';
+import RegistrationForm from "../components/registrationForm";
+import Link from "next/link";
 
 export default function Login() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <RegistrationForm />
-        <div className="mt-4 text-center">
-            <Link
-                href='/login'
-                className="text-blue-600 hover:underline"
-            >
-                Login
+    <>
+      <div
+        className="min-h-screen flex items-center justify-center bg-cover bg-center"
+        style={{
+          backgroundImage: `url('../../../../img/background.jpg')`,
+        }}
+      >
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
+          <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
+            Energy Management System
+          </h1>
+          <RegistrationForm />
+          <div className="mt-4 text-center">
+            <Link href="/login" className="text-blue-600 hover:underline">
+              Login
             </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
