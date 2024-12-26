9a4eb38696028efbcfec390ab981833a3814c0cb
Žiga Črv <62549216+zigac9@users.noreply.github.com>
2024-12-26 19:32:09 +0100

    Always pull images and delete next.config.ts update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "upgrade-insecure-requests",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
