9da13f143ba614d95fb1f27415c204628a3e528a
Žiga Črv <62549216+zigac9@users.noreply.github.com>
2025-01-04 13:47:00 +0100

    Config file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_API_PATH: process.env.NEXT_PUBLIC_API_PATH,
+  },
 };
 
 export default nextConfig;
