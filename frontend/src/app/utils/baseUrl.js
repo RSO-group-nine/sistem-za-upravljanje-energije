3bbbe5487939805cdba4e3f3bdb20feabe4d42f8
Žiga Črv <62549216+zigac9@users.noreply.github.com>
2025-01-06 20:56:36 +0100

    Fix baseUrl

--- frontend/src/app/utils/baseUrl.js
+++ frontend/src/app/utils/baseUrl.js
@@ -1,6 +1,4 @@
-export const baseURL =
-  "http://146.190.206.116:3000/api/v1";
-
+export const baseURL = "http://144.126.246.84:3000/api/v1";
 
 // export const baseURL =
 //   "http://localhost:5000/api/v1";
