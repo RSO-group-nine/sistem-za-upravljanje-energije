a1e12824126a9333059a7dd6dcdd55442edf7bb8
rokrajher <69865825+rokrajher@users.noreply.github.com>
2025-01-09 17:51:10 +0100

    updated base url

--- frontend/src/app/utils/baseUrl.js
+++ frontend/src/app/utils/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://206.189.250.197:3000/api/v1";
+export const baseURL = "http://157.230.78.156:3000/api/v1";
 
 // export const baseURL =
 //   "http://localhost:5000/api/v1";
